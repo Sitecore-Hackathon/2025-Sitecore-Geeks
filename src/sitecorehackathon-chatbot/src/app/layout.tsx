1b83a92e3c0ca6a346a9d58cf897b4722ab7b4c5
Yamini_Punyavathi_Muttevi <yamini_muttevi@epam.com>
2025-03-08 16:45:27 +0530

    Added the Sitecore Geeks Chatbot code files.

---
+++ src/sitecorehackathon-chatbot/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Sitecore Geeks Chatbot",
+  description: "Sitecore Hackathon 2025 - An Application in NEXTJS for integrating the Site Content to a Conversational chatbot.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/sitecorehackathon-chatbot/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import ChatbotComponent  from "./components/ChatbotComponent";
+
+export default function Home() {
+  return (
+  
+ <>
+ <div style={{ backgroundImage: 'url("/logo.png")', height: '620px',width:'800px', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+  
+     <ChatbotComponent />
+  
+    </>
+  );
+}
